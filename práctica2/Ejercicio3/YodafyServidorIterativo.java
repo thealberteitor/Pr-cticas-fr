5763d0327b216c69baaab51a8e798498bf114647
Alberto <albduranlopez@gmail.com>
2017-11-06 18:05:54 +0100

    Añadido ejercicio2

---
+++ práctica2/Ejercicio3/YodafyServidorIterativo.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+//
+// YodafyServidorIterativo
+// (CC) jjramos, 2012
+//
+public class YodafyServidorIterativo {
+
+	public static void main(String[] args) {
+
+		// Puerto de escucha
+		int port=8989;
+		// array de bytes auxiliar para recibir o enviar datos.
+		byte []buffer=new byte[256];
+		// Número de bytes leídos
+		int bytesLeidos=0;
+
+		ServerSocket socketServidor;
+
+		try {
+			socketServidor = new ServerSocket(port);
+
+			Socket socketConexion = null;
+			do {
+				socketConexion = socketServidor.accept();
+
+				ProcesadorYodafy procesador=new ProcesadorYodafy(socketConexion);
+				procesador.procesa();
+
+			} while (true);
+
+		} catch (IOException e) {
+			System.err.println("Error al escuchar en el puerto "+port);
+		}
+
+	}
+
+}
